c199c120eaaebd73767ee8092d6a0356cbe033f0
sheenobu <sheena.artrip@gmail.com>
2015-08-11 22:36:34 -0400

    Updated example

--- src/demos/webframeworks/db.go
+++ src/demos/webframeworks/db.go
@@ -29,9 +29,9 @@ func initDb() {
 	Frameworks = &_Frameworks{
 		Collection:  sql.New(db, "frameworks"),
 		Id:          sql.Column("id", "integer primary key AUTOINCREMENT"), // Should be abstracted out to a sql.PrimaryKeyInteger
-		Name:        sql.Column("name", "varchar(100) not null"),           // Should be abstracted out to a sql.NotNullable(sql.VarChar(100))
-		Description: sql.Column("description", "varchar(100) not null"),    // Should be abstracted out to a sql.NotNullable(sql.VarChar(100))
-		Url:         sql.Column("url", "varchar(100) not null"),            // Should be abstracted out to a sql.NotNullable(sql.VarChar(100))
+		Name:        sql.Varchar("name", 100).NotNull(),
+		Description: sql.Varchar("description", 100).NotNull(),
+		Url:         sql.Varchar("url", 100).NotNull(),
 	}
 
 	// Init performs various operations to analyze columns, create initial schema
